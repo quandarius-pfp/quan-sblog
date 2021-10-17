9323d315b2bb72af41df693c809a57f316051485
quandarius-pfp <78605946+quandarius-pfp@users.noreply.github.com>
2021-10-17 16:31:32 +0700

    oke

--- admincp/config/config.php
+++ admincp/config/config.php
@@ -1,11 +1,21 @@
 <?php 
 
-	$mysqli = new mysqli("localhost","root","","blogs");
+
+
+	$mysqli = new mysqli("sql206.epizy.com","epiz_30088799","2BN6NCu0PTaB","epiz_30088799_blogs");
+
+
 
 	// Check connection
+
 	if ($mysqli->connect_errno) {
-	  echo "Kết nối MYSQLi lỗi" . $mysqli->connect_error;
+
+	  echo "Káº¿t ná»‘i MYSQLi lá»—i" . $mysqli->connect_error;
+
 	  exit();
+
 	}
+
+
 
 ?>
